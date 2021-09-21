8730847c9375b3e7ada40aaaffc773979d956f66
akbarbintangm <akbarbintangmahendra@gmail.com>
2021-09-21 15:15:27 +0700

    update

--- assets/vendor/isotope/bin/version.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const version = require('../package.json').version;
-
-const file = 'js/isotope.js';
-let src = fs.readFileSync( file, 'utf8' );
-src = src.replace( /Isotope v\d+\.\d+\.\d+/, `Isotope v${version}` );
-fs.writeFileSync( file, src, 'utf8' );
